f3e4b31ef748abaf7b8beb79dc7fc93f53cd24d0
yywHost <18206466622@139.com>
2020-02-20 23:01:12 +0800

    0220

--- yywSite/admin.py
+++ yywSite/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from yywSite.models import *
 # Register your models here.
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['id','btitle','bpub_date']
+admin.site.register(BookInfo,BookInfoAdmin)
